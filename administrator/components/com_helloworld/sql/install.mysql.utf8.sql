c68939c6627ce8c38ac7b1706071cede8c73d97a
VladimirVashchenko <vashchenko.vs.88@gmail.com>
2016-07-15 21:44:23 +0300

    finished

--- administrator/components/com_helloworld/sql/install.mysql.utf8.sql
+++ administrator/components/com_helloworld/sql/install.mysql.utf8.sql
@@ -3,11 +3,14 @@ DROP TABLE IF EXISTS `#__helloworld`;
 CREATE TABLE `#__helloworld` (
 	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
 	`asset_id` INT(10)     NOT NULL DEFAULT '0',
-	`catid`	    int(11)    NOT NULL DEFAULT '0',
-	`menuitem` int(11)    NOT NULL,
+	`catid`	    INT(11)    NOT NULL DEFAULT '0',
+	`menuitem` INT(11)    NOT NULL,
 	`greeting` VARCHAR(25) NOT NULL,
-	`published` tinyint(4) NOT NULL,
 	`svgpath` TEXT NOT NULL,
+	`viewportwidth` INT(10) NOT NULL DEFAULT '0',
+	`viewportheight` INT(10) NOT NULL DEFAULT '0',
+	`viewboxwidth` INT(10) NOT NULL DEFAULT '0',
+	`viewboxheight` INT(10) NOT NULL DEFAULT '0',
 	`blockmessage` TEXT NOT NULL,
 	`params`   VARCHAR(1024) NOT NULL DEFAULT '',
 	`state` tinyint(1) NOT NULL default '0',
@@ -18,7 +21,3 @@ CREATE TABLE `#__helloworld` (
 	ENGINE =MyISAM
 	AUTO_INCREMENT =0
 	DEFAULT CHARSET =utf8;
- 
-INSERT INTO `#__helloworld` (`greeting`) VALUES
-('Hello World!'),
-('Good bye World!');

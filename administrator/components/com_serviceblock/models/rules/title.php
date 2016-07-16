f8cb880cc6108b1a6ea62c5cd54ca0080d7be239
VladimirVashchenko <vashchenko.vs.88@gmail.com>
2016-07-16 16:20:25 +0300

    General cleanup

---
+++ administrator/components/com_serviceblock/models/rules/title.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 06.07.2016
+ * Time: 03:55
+ */
+defined('_JEXEC') or die;
+
+/**
+ * Form Rule class for the Joomla Framework.
+ */
+class JFormRuleTitle extends JFormRule
+{
+    /**
+     * The regular expression.
+     *
+     * @access	protected
+     * @var		string
+     * @since	2.5
+     */
+    protected $regex = '^[^0-9]+$';
+}
